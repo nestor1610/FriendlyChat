47954264fe04cadfa6f406321c4af2a5e3ce6a59
nestor161095 <tico1610@gmail.com>
2022-11-16 13:54:10 -0400

    chat amigable!!!

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -7,13 +7,19 @@
  * 4. Copy the config object snippet, then add it here.
  */
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyCyx3DLXN8uEdIKD4K8zUp0J8vAh6qLcmQ",
+  authDomain: "friendlychat-b5f14.firebaseapp.com",
+  projectId: "friendlychat-b5f14",
+  storageBucket: "friendlychat-b5f14.appspot.com",
+  messagingSenderId: "854838812855",
+  appId: "1:854838812855:web:3708d3a5ee65d86f912d32"
 };
+
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
     throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
+      'Add your web app\'s configuration object to firebase-config.js');
   } else {
     return config;
   }
